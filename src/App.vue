1828718abf729b7b0ca53b45d219c3d68f6258b4
scarlett-troncoso <scar.labussieri@gmail.com>
2024-01-25 13:14:24 +0100

    add image

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,30 @@ export default {
   name: "App",
   data() {
     return {
-      message: 'CIAO A TUTTI ☻'
+      message: 'CIAO A TUTTI ☻',
+      image: 'universo.jpg',
+      classeImg: 'sizeImg'
+    };
+  },
+
+  methods: {
+    imagePath: function (img) {
+      return new URL(`./assets/${img}`,
+        import.meta.url).href;
     }
   }
-}
+};
 </script>
 
 <template>
   <div>
     <h1>{{ message }}</h1>
   </div>
+
+  <div>
+    <img :src="imagePath(image)" alt="" v-bind:class="classeImg">
+  </div>
 </template>
+
 
 <style></style>
